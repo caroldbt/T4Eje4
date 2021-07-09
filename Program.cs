332bdb52392a0eb20136e8d133f80e4c3bef04f5
caroldbt <37454302+caroldbt@users.noreply.github.com>
2021-07-09 22:26:48 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,40 @@ namespace T4Eje4
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numero1 = 0, numero2 = 0;
+            bool entrada = false;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Ingrese el primer número");
+                    numero1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ingrese el siguiente número");
+                    numero2 = Convert.ToInt32(Console.ReadLine());
+                    entrada = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Valor introducido no es un entero");
+                }
+
+            } while (!entrada);
+
+            if (entrada)
+            {
+                if (numero1 > numero2)
+                {
+                    Console.WriteLine("El número " + numero1 + " es mayor.");
+                }
+                else if (numero2 > numero1)
+                {
+                    Console.WriteLine("El numero " + numero2 + " es mayor.");
+                }
+                else
+                {
+                    Console.WriteLine("Son iguales");
+                }
+            }
         }
     }
 }
